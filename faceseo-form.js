d38c6ac1c422d2a10c946aeb997962b9469717c9
huynhthanhlam100692 <52100703+huynhthanhlam100692@users.noreply.github.com>
2019-06-25 21:06:38 +0700

    Update faceseo-form.js

--- faceseo-form.js
+++ faceseo-form.js
@@ -111,7 +111,7 @@ jQuery(divpopup).attr('id', 'myModalthanh');
 jQuery(divpopup).attr('class', 'thanhfaceseo-modal');
 jQuery( "body" ).append(divpopup);
 var titleform='Đăng ký Form';
-var urlform="https://docs.google.com/forms/d/e/1FAIpQLSfbun6UwylPJtGb9-UM0d0HIrumLitia5y4J19HhChAWYKD0A/formResponse";
+var urlform="https://docs.google.com/forms/d/1m3v53lDEOoIYGkSK4UqkyiET54IJ-WzSv59C5nCxuyc/edit";
 var urlchuyen=window.location.href;
 var hovaten="1353113421";
 var dienthoai="1192133035";
